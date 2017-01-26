f9e13cb0eba7eaee1762bd8e3802e6337acaf7d5
unknown <lijot4@lijot4.nti.umea>
2017-01-26 09:39:59 +0100

    basic camera

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -43,4 +43,23 @@ var app = {
     }
 };
 
+
+document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
+function cameraTakePicture() {
+   navigator.camera.getPicture(onSuccess, onFail, { 
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+   });
+
+   function onSuccess(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+   }
+
+   function onFail(message) {
+      alert('Failed because: ' + message);
+   }
+}
+
+
 app.initialize();
